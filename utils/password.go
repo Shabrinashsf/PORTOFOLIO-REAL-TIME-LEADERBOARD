7a6aba3e800a83c60484f5381bbfa6a29ee7ca65
Shabrina Amalia Safaana <shabrinaamalia860@gmail.com>
2025-07-18 22:20:53 +0800

    feat: add response, password hashing, and password encode

---
+++ utils/password.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"PORTOFOLIO-REAL-TIME-LEADERBOARD/dto"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashPass, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		return "", dto.ErrHashPass
+	}
+
+	return string(hashPass), nil
+}
+
+func VerifyPassword(userPass string, reqPass string) error {
+	return bcrypt.CompareHashAndPassword([]byte(userPass), []byte(reqPass))
+}
